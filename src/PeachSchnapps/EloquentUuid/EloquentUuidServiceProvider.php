e80c5f0503aed877503fd77f2484dec57f968de2
electblake <electblake@gmail.com>
2013-09-07 13:46:17 -0400

    init commit

---
+++ src/PeachSchnapps/EloquentUuid/EloquentUuidServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace PeachSchnapps\EloquentUuid;
+
+use Illuminate\Support\ServiceProvider;
+
+class EloquentUuidServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('peach-schnapps/eloquent-uuid');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
